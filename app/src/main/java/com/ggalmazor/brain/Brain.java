e6293fbc6a4d96eb2936963e55139759968c37cd
Guillermo Gutierrez Almazor <ggalmazor@gmail.com>
2024-10-05 22:00:50 +0200

    Cleanup: Add Javadoc

--- app/src/main/java/com/ggalmazor/brain/Brain.java
+++ app/src/main/java/com/ggalmazor/brain/Brain.java
@@ -7,6 +7,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * Knows how to describe an acronym and remembers their descriptions after that
+ */
 public class Brain {
     private final Acronyms acronyms;
     private final Map<String, List<String>> memory;
@@ -16,14 +19,24 @@ public class Brain {
         this.memory = memory;
     }
 
+    /**
+     * Returns a Brain that doesn't know any acronym yet
+     */
     static Brain empty(Acronyms acronyms) {
         return new Brain(acronyms, new ConcurrentHashMap<>());
     }
 
+    /**
+     * Returns a Brain that will return random acronym descriptions
+     */
     public static Brain random() {
         return new Brain(new RandomAcronyms(), new ConcurrentHashMap<>());
     }
 
+    /**
+     * Returns the memorized descriptions of the provided acronym. When there's no memory of it,
+     * generates a new acronym using the Acronym source.
+     */
     public List<String> getDescriptions(String acronym) {
         return memory.computeIfAbsent(acronym, key -> new ArrayList<>(List.of(acronyms.getDescription(acronym))));
     }
