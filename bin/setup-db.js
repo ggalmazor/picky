b924dd01086cb34b6a2c84d9da2b3bbfc4067ce1
Guillermo Gutierrez Almazor <ggalmazor@gmail.com>
2024-10-19 20:27:51 +0200

    Change: tweak setup db script
    Connecting without a database name won't work in Heroku, which means that we need to manually check the presence of the database instead.
    
    Also, ensure that the db connections are always destroyed

--- bin/setup-db.js
+++ bin/setup-db.js
@@ -2,45 +2,63 @@ import knex from 'knex';
 import profiles from '../knexfile.js';
 
 function withoutDatabaseName(profile) {
-  const config = JSON.parse(JSON.stringify(profile));
-  const configConnection = config.connection;
-  delete configConnection.database;
-  config.connection = configConnection;
-  return config;
+  return {
+    client: profile.client,
+    connection: {
+      host: profile.connection.host,
+      port: profile.connection.port,
+      user: profile.connection.user,
+      password: profile.connection.password,
+    },
+  };
+}
+
+async function databaseMustBeCreated(profile) {
+  let db;
+  try {
+    db = knex(profile);
+    await db.raw('select 1+1');
+    console.log(`ℹ️Database ${profile.connection.database} already exists`);
+    return false;
+  } catch (error) {
+    if (error.message === `database "${profile.connection.database}" does not exist`) return true;
+
+    throw error;
+  } finally {
+    await db.destroy();
+  }
 }
 
 async function createDatabase(profile) {
-  const db = knex(withoutDatabaseName(profile));
+  let db;
   try {
+    db = knex(withoutDatabaseName(profile));
     await db.raw(`CREATE DATABASE ${profile.connection.database}`);
-    console.log(`✅ Database ${profile.connection.database} created`)
-  } catch (error) {
-    if (error.message.includes(`database "${profile.connection.database}" already exists`)) {
-      console.log(`ℹ️Database ${profile.connection.database} already exists`);
-    } else {
-      throw error;
-    }
+    console.log(`✅ Database ${profile.connection.database} created`);
+    await db.destroy();
+  } finally {
+    await db.destroy();
   }
-  await db.destroy();
 }
 
 async function runMigrations(profile) {
   const db = knex(profile);
   try {
     await db.migrate.latest();
-    console.log(`✅ Migrations executed in ${profile.connection.database}`)
+    console.log(`✅ Migrations executed in ${profile.connection.database}`);
   } catch (error) {
     console.error(error);
+  } finally {
+    await db.destroy();
   }
-  await db.destroy();
 }
 
 const environment = process.env.NODE_ENV;
 
 console.log(`Setting up DB for env: ${environment}`);
 
-if (!(environment in profiles))
-  throw new Error(`No profile for ${environment} environment`);
+if (!(environment in profiles)) throw new Error(`No profile for ${environment} environment`);
 
-await createDatabase(profiles[environment]);
+if (await databaseMustBeCreated(profiles[environment])) await createDatabase(profiles[environment]);
+
 await runMigrations(profiles[environment]);
