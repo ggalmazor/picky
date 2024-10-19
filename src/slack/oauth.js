cf348ceb5205f09742ef4d0acc79b3e37e6149ad
Guillermo Gutierrez Almazor <ggalmazor@gmail.com>
2024-10-19 13:38:34 +0200

    Change: create Slack Oauth class to exchange codes for access tokens within the OAuth flow

---
+++ src/slack/oauth.js
@@ -0,0 +1,17 @@
+export default class SlackOAuth {
+  constructor(client, credentials) {
+    this.client = client;
+    this.credentials = credentials;
+  }
+
+  async access(code) {
+    const response = await this.client.oauth.v2.access({
+      client_id: this.credentials.id,
+      client_secret: this.credentials.secret,
+      code
+    });
+    const {access_token: accessToken, team, enterprise} = response;
+    return {accessToken, team, enterprise};
+  }
+
+}
