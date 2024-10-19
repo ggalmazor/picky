510c226d1307af7bd0199389ab4ee04b15b5a66e
Guillermo Gutierrez Almazor <ggalmazor@gmail.com>
2024-10-19 17:55:20 +0200

    Change: build message for TeamNeedsSetUpError errors

--- src/errors/errors.js
+++ src/errors/errors.js
@@ -1,1 +1,5 @@
-export class TeamNeedsSetUpError extends Error {}
+export class TeamNeedsSetUpError extends Error {
+  constructor(slackEnterpriseId, slackTeamId) {
+    super(`A team must be set up for Slack team ${slackTeamId} at ${slackEnterpriseId === undefined ? 'N/A' : slackEnterpriseId}`);
+  }
+}
