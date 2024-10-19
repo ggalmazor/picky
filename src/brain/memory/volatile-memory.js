b274ad8f8a3927ba9195bf21780e705b2c448ffd
Guillermo Gutierrez Almazor <ggalmazor@gmail.com>
2024-10-19 10:40:29 +0200

    Change: adapt the VolatileMemory implementation

--- src/brain/memory/volatile-memory.js
+++ src/brain/memory/volatile-memory.js
@@ -3,25 +3,28 @@ export default class VolatileMemory {
     this.data = data;
   }
 
-  async knows(acronym, definition) {
+  async knows(context, acronym, definition) {
     if (definition === undefined) return Promise.resolve(acronym in this.data);
 
     return Promise.resolve(acronym in this.data && this.data[acronym].includes(definition));
   }
 
-  async recall(acronym) {
+  async recall(context, acronym) {
     return Promise.resolve(this.data[acronym] || []);
   }
 
-  async learn(acronym, definition) {
+  async learn(context, acronym, definition) {
     if (await this.knows(acronym, definition)) return Promise.resolve();
 
     this.data[acronym] ||= [];
+    if (this.data[acronym].includes(definition))
+      return Promise.resolve();
+
     this.data[acronym].push(definition);
     return Promise.resolve();
   }
 
-  async forget(acronym, definition) {
+  async forget(context, acronym, definition) {
     if (!(acronym in this.data)) return Promise.resolve();
     this.data[acronym] = this.data[acronym].filter((def) => def !== definition);
     if (this.data[acronym].length === 0) delete this.data[acronym];
