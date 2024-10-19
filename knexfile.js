62963e5577abcfecbe38a91afffcb862643fc9f3
Guillermo Gutierrez Almazor <ggalmazor@gmail.com>
2024-10-19 19:47:56 +0200

    Change: set up the production DB profile

--- knexfile.js
+++ knexfile.js
@@ -22,4 +22,15 @@ export default {
       database: process.env.DATABASE_NAME,
     },
   },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+    },
+  },
 };
