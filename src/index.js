38eece07cb4c813f1abe43766f3bc1c5511ed714
Guillermo Gutierrez Almazor <ggalmazor@gmail.com>
2024-10-20 11:05:38 +0200

    Change: temporarily debug the uninstall event

--- src/index.js
+++ src/index.js
@@ -71,6 +71,7 @@ async function init() {
   console.log('👂app home opened listener registered');
 
   app.event('app_uninstalled', async (payload) => {
+    app.logger.debug(payload.event);
     await installer.uninstall(payload.event.team_id).catch((error) => app.logger.error(error.stack));
 
     app.logger.info(`⚠️ Team ${payload.event.team_id} uninstalled`);
